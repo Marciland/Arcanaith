f4490e3a8f8f92362e8c6e62dae78af8c21b6f10
Marcel Witoschek <92092148+Marciland@users.noreply.github.com>
2024-11-14 17:27:46 +0100

    Game-loading

--- src/ecs/entity/loader/game/mod.rs
+++ src/ecs/entity/loader/game/mod.rs
@@ -5,36 +5,42 @@ use crate::ecs::{
     entity::{EntityLoader, EntityManager},
 };
 use glam::Vec3;
+use overlay::OverlayLoader;
 
 pub fn load_new_game(loader: &mut EntityLoader, entity_manager: &mut EntityManager) {
     loader.component_manager.player_entity = Some(player::create(loader, entity_manager));
-    /*
-        let background = entity_manager.create_entity();
-        loader.component_manager.position_storage.add(
-            background,
-            PositionComponent {
-                xyz: Vec3 {
-                    x: 0.0,
-                    y: 0.0,
-                    z: 0.0,
-                },
-                scale: Vec3 {
-                    x: 2.0,
-                    y: 2.0,
-                    z: 1.0,
-                },
-            },
-        );
-        loader.component_manager.visual_storage.add(
-            background,
-            VisualComponent::new(
-                vec![loader.resource_system.get_texture_index("game_background")],
-                Layer::Background,
-                0,
-            ),
-        );
-    */
-    overlay::load(loader, entity_manager);
 
-    let _monster_spawner = entity_manager.create_entity();
+    // TODO animate background and never move it instead?
+    let background = entity_manager.create_entity();
+    loader.component_manager.position_storage.add(
+        background,
+        PositionComponent {
+            xyz: Vec3 {
+                x: 0.0,
+                y: 0.0,
+                z: 0.0,
+            },
+            scale: Vec3 {
+                x: 2.0,
+                y: 2.0,
+                z: 1.0,
+            },
+        },
+    );
+    loader.component_manager.visual_storage.add(
+        background,
+        VisualComponent::new(
+            vec![loader.resource_system.get_texture_index("game_background")],
+            Layer::Background,
+            0,
+        ),
+    );
+
+    OverlayLoader {
+        loader,
+        entity_manager,
+    }
+    .load();
+
+    todo!("spawn waves mechanic")
 }
--- src/ecs/entity/loader/game/overlay.rs
+++ src/ecs/entity/loader/game/overlay.rs
@@ -30,7 +30,7 @@ impl<'loading> EntityLoader<'loading> {
         texture: &str,
         xyz: Vec3,
         scale: Vec3,
-        input: InputComponent,
+        input: fn(&EventLoopProxy<GameEvent>) -> (),
     ) {
         self.component_manager.visual_storage.add(
             entity,
@@ -45,118 +45,212 @@ impl<'loading> EntityLoader<'loading> {
             .position_storage
             .add(entity, PositionComponent { xyz, scale });
 
-        self.component_manager.input_storage.add(entity, input);
+        self.component_manager.input_storage.add(
+            entity,
+            InputComponent {
+                is_active: false,
+                previous: entity,
+                next: entity,
+                activate: input,
+            },
+        );
     }
 }
 
-pub fn load(loader: &mut EntityLoader, entity_manager: &mut EntityManager) {
-    let health_bar = entity_manager.create_entity();
-    loader.create_empty_bar(
-        health_bar,
-        Vec3 {
-            x: -0.925,
-            y: 0.925,
-            z: 0.0,
-        },
-        Vec3 {
-            x: 0.15,
-            y: 0.05,
-            z: 1.0,
-        },
-    );
+pub struct OverlayLoader<'overlay, 'loader> {
+    pub loader: &'overlay mut EntityLoader<'loader>,
+    pub entity_manager: &'overlay mut EntityManager,
+}
 
-    let mana_bar = entity_manager.create_entity();
-    loader.create_empty_bar(
-        mana_bar,
-        Vec3 {
-            x: -0.925,
-            y: 0.975,
-            z: 0.0,
-        },
-        Vec3 {
-            x: 0.15,
-            y: 0.05,
-            z: 1.0,
-        },
-    );
+impl<'overlay, 'loader> OverlayLoader<'overlay, 'loader> {
+    pub fn load(&mut self) {
+        self.create_health_bar();
+        self.create_mana_bar();
+        self.create_exp_bar();
+        self.create_money();
+        self.create_inventory();
+        self.create_wave_counter();
+        self.create_highscore();
+        self.create_pause();
+    }
 
-    let exp_bar = entity_manager.create_entity();
-    loader.create_empty_bar(
-        exp_bar,
-        Vec3 {
-            x: 0.0,
-            y: 0.975,
-            z: 0.0,
-        },
-        Vec3 {
-            x: 0.6,
-            y: 0.05,
-            z: 1.0,
-        },
-    );
-
-    let money = entity_manager.create_entity();
-    loader.component_manager.visual_storage.add(
-        money,
-        VisualComponent::new(
-            vec![loader.resource_system.get_texture_index("money_bag")],
-            Layer::Interface,
-            0,
-        ),
-    );
-    loader.component_manager.position_storage.add(
-        money,
-        PositionComponent {
-            xyz: Vec3 {
-                x: 0.7,
-                y: 0.975,
+    fn create_health_bar(&mut self) {
+        let health_bar = self.entity_manager.create_entity();
+        self.loader.create_empty_bar(
+            health_bar,
+            Vec3 {
+                x: -0.925,
+                y: 0.925,
                 z: 0.0,
             },
-            scale: Vec3 {
-                x: 0.1,
+            Vec3 {
+                x: 0.15,
                 y: 0.05,
                 z: 1.0,
             },
-        },
-    );
+        );
+    }
 
-    let bag = entity_manager.create_entity();
-    loader.create_clickable(
-        bag,
-        "bag",
-        Vec3 {
-            x: 0.85,
-            y: 0.975,
-            z: 0.0,
-        },
-        Vec3 {
-            x: 0.1,
-            y: 0.1,
-            z: 1.0,
-        },
-        InputComponent {
-            is_active: true,
-            previous: bag,
-            next: bag,
-            activate: open_inventory,
-        },
-    );
-    /*
-    let pause = entity_manager.create_entity();
-    loader.create_clickable(
-        pause,
-        Vec3 {
-            x: (),
-            y: (),
-            z: (),
-        },
-        Vec3 {
-            x: (),
-            y: (),
-            z: (),
-        },
-    );
-    */
+    fn create_mana_bar(&mut self) {
+        let mana_bar = self.entity_manager.create_entity();
+        self.loader.create_empty_bar(
+            mana_bar,
+            Vec3 {
+                x: -0.925,
+                y: 0.975,
+                z: 0.0,
+            },
+            Vec3 {
+                x: 0.15,
+                y: 0.05,
+                z: 1.0,
+            },
+        );
+    }
+
+    fn create_exp_bar(&mut self) {
+        let exp_bar = self.entity_manager.create_entity();
+        self.loader.create_empty_bar(
+            exp_bar,
+            Vec3 {
+                x: 0.0,
+                y: 0.975,
+                z: 0.0,
+            },
+            Vec3 {
+                x: 0.6,
+                y: 0.05,
+                z: 1.0,
+            },
+        );
+    }
+
+    fn create_money(&mut self) {
+        let money = self.entity_manager.create_entity();
+        self.loader.component_manager.visual_storage.add(
+            money,
+            VisualComponent::new(
+                vec![self.loader.resource_system.get_texture_index("money_bag")],
+                Layer::Interface,
+                0,
+            ),
+        );
+        self.loader.component_manager.position_storage.add(
+            money,
+            PositionComponent {
+                xyz: Vec3 {
+                    x: 0.7,
+                    y: 0.975,
+                    z: 0.0,
+                },
+                scale: Vec3 {
+                    x: 0.1,
+                    y: 0.05,
+                    z: 1.0,
+                },
+            },
+        );
+    }
+
+    fn create_inventory(&mut self) {
+        let inventory = self.entity_manager.create_entity();
+        self.loader.create_clickable(
+            inventory,
+            "bag",
+            Vec3 {
+                x: 0.85,
+                y: 0.975,
+                z: 0.0,
+            },
+            Vec3 {
+                x: 0.1,
+                y: 0.1,
+                z: 1.0,
+            },
+            open_inventory,
+        );
+    }
+
+    fn create_wave_counter(&mut self) {
+        let wave_counter = self.entity_manager.create_entity();
+        self.loader.component_manager.visual_storage.add(
+            wave_counter,
+            VisualComponent::new(
+                vec![self
+                    .loader
+                    .resource_system
+                    .get_texture_index("wave_counter")],
+                Layer::Interface,
+                0,
+            ),
+        );
+        self.loader.component_manager.position_storage.add(
+            wave_counter,
+            PositionComponent {
+                xyz: Vec3 {
+                    x: 0.0,
+                    y: -0.8,
+                    z: 0.0,
+                },
+                scale: Vec3 {
+                    x: 0.6,
+                    y: 0.1,
+                    z: 1.0,
+                },
+            },
+        );
+    }
+
+    fn create_highscore(&mut self) {
+        let highscore = self.entity_manager.create_entity();
+        self.loader.component_manager.visual_storage.add(
+            highscore,
+            VisualComponent::new(
+                vec![self.loader.resource_system.get_texture_index("highscore")],
+                Layer::Interface,
+                0,
+            ),
+        );
+        self.loader.component_manager.position_storage.add(
+            highscore,
+            PositionComponent {
+                xyz: Vec3 {
+                    x: 0.0,
+                    y: -0.9,
+                    z: 0.0,
+                },
+                scale: Vec3 {
+                    x: 0.6,
+                    y: 0.1,
+                    z: 1.0,
+                },
+            },
+        );
+    }
+    fn create_pause(&mut self) {
+        let pause_button = self.entity_manager.create_entity();
+        self.loader.create_clickable(
+            pause_button,
+            "pause_button",
+            Vec3 {
+                x: -0.925,
+                y: -0.925,
+                z: 0.0,
+            },
+            Vec3 {
+                x: 0.05,
+                y: 0.05,
+                z: 1.0,
+            },
+            pause_clicked,
+        );
+    }
 }
 
-fn open_inventory(event_proxy: &EventLoopProxy<GameEvent>) {}
+fn pause_clicked(_event_proxy: &EventLoopProxy<GameEvent>) {
+    todo!("pause clicked")
+}
+
+fn open_inventory(_event_proxy: &EventLoopProxy<GameEvent>) {
+    todo!("open inventory")
+}

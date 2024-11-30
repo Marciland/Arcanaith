0a74bb760e4b273b816baa196bd03f68a04c6fb8
Marcel Witoschek <marcel.marciland@gmail.com>
2024-11-30 23:39:32 +0100

    .

--- scripts/compile_shaders.ps1
+++ scripts/compile_shaders.ps1
@@ -1,6 +1,6 @@
 $glslc = Get-ChildItem "C:\VulkanSDK\*\Bin\glslc.exe" -File | Select-Object -First 1
 
 if ($glslc) {
-    & $glslc.FullName game\shaders\shader.vert -o game\shaders\vert.spv
-    & $glslc.FullName game\shaders\shader.frag -o game\shaders\frag.spv
+    & $glslc.FullName shaders\shader.vert -o shaders\vert.spv
+    & $glslc.FullName shaders\shader.frag -o shaders\frag.spv
 }
